9e5e1810263b8eab0d4438accc6578a0bfb5d909
Zhang Yuhan <yuhanz@gmail.com>
2019-09-21 15:57:47 -0700

    refactor

--- gpio_dc_control.py
+++ gpio_dc_control.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 
 class PwmDcControl:
-    def __init__(self, name, ena, in1, in2, pwmFrequency, pwdDutyCycle = 0, inputVoltage, dcMaxVoltage):
-        self.name
+    def __init__(self, name, ena, in1, in2, inputVoltage, dcMaxVoltage, pwmFrequency, pwmDuty = 0):
+        self.name = name
         self.ena = ena
         self.in1 = in1
         self.in2 = in2
@@ -11,82 +11,47 @@ class PwmDcControl:
         GPIO.setup(ena,GPIO.OUT)
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.LOW)
-        self.inputStates = [GPIO.LOW, GPIO.LOW]
-
         self.pwm = GPIO.PWM(ena, pwmFrequency)
+        self.pwmFrequency = pwmFrequency
+        self.inputStates = [GPIO.LOW, GPIO.LOW]
         self.maxDuty = 100 * dcMaxVoltage / inputVoltage
 
-        self.validateDuty(pwdDutyCycle)
-        self.pwm.start(pwdDutyCycle)
-
+        self.validateDuty(pwmDuty)
+        self.pwm.start(pwmDuty)
+        self.pwmDuty = pwmDuty
+        print("init duty={}".format(pwmDuty))
+    def toString(self):
+        return "name={}, pins(ena,in1,in2)={}, pwm(frequency,duty)={}, constraint(maxDuty)={}".format(self.name, (self.ena, self.in1, self.in2), (self.pwmFrequency,self.pwmDuty), self.maxDuty)
     def setInputState(self, desiredInputStates):
-        if(self.inputStates == desiredInputStates)
+        if(self.inputStates == desiredInputStates):
             return
-        map(lambda pair: GPIO.outout(pair[0], pair[1]), zip([self.in1, self.in2], desiredInputStates))
+        map(lambda pair: GPIO.output(pair[0], pair[1]), zip([self.in1, self.in2], desiredInputStates))
         self.inputStates = desiredInputStates
-
+    def displayStatus(self, status):
+        print("{}:{}".format(self.name, self.status))
     def backward(self):
         self.setInputState([GPIO.HIGH, GPIO.LOW])
-        print("backward")
-
+        self.displayStatus("backward")
     def forward(self):
         self.setInputState([GPIO.LOW, GPIO.HIGH])
-        print("forward")
-
+        self.displayStatus("forward")
     def stop(self):
         self.setInputState([GPIO.LOW, GPIO.LOW])
-        print("stop")
-
+        self.displayStatus("stop")
     def setPwmFrequency(self, frequency):
         self.pwm.ChangeFrequency(frequncy)
-
     def capDuty(self, duty):
         cappedDuty = reduce(lambda a,b: min(a,b), [self.maxDuty, 100], duty)
         return max(cappedDuty, 0)
-
     def validateDuty(self, duty):
         if(duty > self.maxDuty):
             raise Exception("duty={} is above the maxDuty={} for {}".format(duty, maxDuty, name))
-
     def setPwmDuty(self, duty):
         self.validateDuty(duty)
-        self.pwm.ChangeDuty(capDuty(duty))
-
+        print("changeDuty={}".format(duty))
+        self.pwm.ChangeDutyCycle(capDuty(duty))
+    # Adjust motor strength between [0, 1] at the safe voltage range
     def setStrength(self, rate):
         if(rate <0 or rate > 1):
             raise Exception("rate must be between [0,1]")
         self.setPwmDuty(rate * self.maxDuty)
-
-
-PWM_FREQUENCY = 500
-PWM_INITIAL_DUTY = 15
-MOTOR_INPUT_VOLTAGE = 9
-DC_MAX_VOLTAGE = 3
-
-GPIO.setmode(GPIO.BCM)
-
-pwdControlWheelLeft = PwmDcControl(ena = 25, in1 = 24, in2 = 23, pwmFrequency = PWM_FREQUENCY, pwmDuty = PWM_INITIAL_DUTY, inputVoltage = MOTOR_INPUT_VOLTAGE, dcMaxVoltage = DC_MAX_VOLTAGE)
-pwdControlWheelRight = PwmDcControl(ena = 9, in1 = 7, in2 = 8, pwmFrequency = PWM_FREQUENCY, pwmDuty = PWM_INITIAL_DUTY, inputVoltage = MOTOR_INPUT_VOLTAGE, dcMaxVoltage = DC_MAX_VOLTAGE)
-
-def turnLeft():
-    pwdControlWheelLeft.back()
-    pwdControlWheelRight.forward()
-
-def turnRight():
-    pwdControlWheelLeft.forward()
-    pwdControlWheelRight.backward()
-
-def moveForward():
-    pwdControlWheelLeft.forward()
-    pwdControlWheelRight.forward()
-
-def stop():
-    pwdControlWheelLeft.stop()
-    pwdControlWheelRight.stop()
-
-def setSpeed(rate):
-    pwdControlWheelLeft.setStrength(rate);
-    pwdControlWheelRight.setStrength(rate);
-
-
-GPIO.cleanup()
---
+++ main.py
@@ -0,0 +1,41 @@
+from gpio_dc_control import *
+
+PWM_FREQUENCY = 500
+PWM_INITIAL_DUTY = 15
+MOTOR_INPUT_VOLTAGE = 9
+DC_MAX_VOLTAGE = 3
+
+GPIO.setmode(GPIO.BCM)
+
+pwmControlWheelRight = PwmDcControl(name = "rightWheel", ena = 25, in1 = 24, in2 = 23, pwmFrequency = PWM_FREQUENCY, pwmDuty = PWM_INITIAL_DUTY, inputVoltage = MOTOR_INPUT_VOLTAGE, dcMaxVoltage = DC_MAX_VOLTAGE)
+pwmControlWheelLeft = PwmDcControl(name = "leftWheel", ena = 9, in1 = 7, in2 = 8, pwmFrequency = PWM_FREQUENCY, pwmDuty = PWM_INITIAL_DUTY, inputVoltage = MOTOR_INPUT_VOLTAGE, dcMaxVoltage = DC_MAX_VOLTAGE)
+
+print("Wheel configurations:")
+print(pwmControlWheelRight.toString())
+print(pwmControlWheelLeft.toString())
+
+def turnLeft():
+    pwmControlWheelLeft.backward()
+    pwmControlWheelRight.forward()
+
+def turnRight():
+    pwmControlWheelLeft.forward()
+    pwmControlWheelRight.backward()
+
+def moveForward():
+    pwmControlWheelLeft.forward()
+    pwmControlWheelRight.forward()
+def moveBackward():
+    pwmControlWheelLeft.backward()
+    pwmControlWheelRight.backward()
+
+def stop():
+    pwmControlWheelLeft.stop()
+    pwmControlWheelRight.stop()
+
+def setSpeed(rate):
+    pwmControlWheelLeft.setStrength(rate);
+    pwmControlWheelRight.setStrength(rate);
+
+
+GPIO.cleanup()
