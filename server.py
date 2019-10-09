0050b690b73810f5710f6e1dcc03cba2be3365da
Zhang Yuhan <yuhanz@gmail.com>
2019-10-09 00:07:13 -0700

    initial setup of the web service endpoint to control the motor

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from rover_control import *
+
+app = Flask(__name__)
+
+motorMoveFunctionMap = {
+    'stop' : stop,
+    'forward': moveForward,
+    'backward': moveBackward,
+    'left': turnLeft,
+    'right': turnRight
+}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/motor/<move>')
+def motorMoveEndpoint():
+    move = request.view_args['move']
+    func = motorMoveFunctionMap.get(move, None)
+    if func:
+        func()
+        return '{"status": "OK"}'
+    else:
+        return '{"status": "not found"}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
