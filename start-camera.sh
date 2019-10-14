b4e95a7dc80f13efb79476a9d578e12ae05a2ebe
Zhang Yuhan <yuhanz@gmail.com>
2019-10-14 01:16:00 -0700

    flip camera vertically

--- start-camera.sh
+++ start-camera.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd ~/jsmpeg && node websocket-relay.js supersecret 8081 8082 > /tmp/relay.txt &
-ffmpeg -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 http://localhost:8081/supersecret > /tmp/video.txt &
+ffmpeg -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 -f mpegts -vf vflip -codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 http://localhost:8081/supersecret > /tmp/video.txt &
